29cba6498acff422bd20e9f46dfacb916e130ea2
Martin Burchard <dev@burchard-it.de>
2022-04-14 16:23:12 +0200

    change to `script setup` style

--- src/vue/components/HelloWorld.vue
+++ src/vue/components/HelloWorld.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+
+// eslint-disable-next-line no-undef
+defineProps({
+  msg: String,
+});
+</script>
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
@@ -32,17 +39,6 @@
     </ul>
   </div>
 </template>
-
-<script lang="ts">
-import {defineComponent} from 'vue';
-
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-});
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
