6172c63ba1bb72a42bbbb5734312eff7e93d460c
Martin Burchard <dev@burchard-it.de>
2022-04-14 15:57:12 +0200

    preparation for planed features

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  modulePathIgnorePatterns: [
+    '<rootDir>\\\\(?:build|dist_electron|node_modules)\\\\',
+  ],
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  testMatch: ['<rootDir>/tests/**/*.spec.[jt]s?(x)'],
 };
