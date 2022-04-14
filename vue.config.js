a1194e4b78e20be14bfaf11dcea924626433fa82
Martin Burchard <dev@burchard-it.de>
2022-04-14 16:15:23 +0200

    move Electron specific source code to a sub folder

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const {defineConfig} = require('@vue/cli-service');
 module.exports = defineConfig({
+  pluginOptions: {
+    electronBuilder: {
+      mainProcessFile: 'src/electron',
+    },
+  },
   transpileDependencies: true,
 });
