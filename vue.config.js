99f908d5b69fd724aea520093a3ec4e1ac1a7df4
Martin Burchard <dev@burchard-it.de>
2022-04-14 16:19:39 +0200

    move Vue (Frontend) specific source code to a sub folder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const {defineConfig} = require('@vue/cli-service');
 module.exports = defineConfig({
+  pages: {
+    index: {
+      entry: 'src/vue',
+    },
+  },
   pluginOptions: {
     electronBuilder: {
       mainProcessFile: 'src/electron',
